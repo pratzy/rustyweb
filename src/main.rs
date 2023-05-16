a3ace9734aedad7c3038e80caf6d2ac71e5ec84d
Prateek <mail.prateek.agarwal@gmail.com>
2023-05-16 17:40:34 +0800

    More code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-use rustywebs::ThreadPool;
+use rustyweb::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
 };
 
-fn main() {
+fn main() -> std::io::Result<()>{
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        pool.execute(|| handle_connection(stream));
+        match stream {
+            Ok(stream) => {
+                pool.execute(|| handle_connection(stream));
+            }
+            Err(e) => {
+                panic!("Error in receiving connection {:?}", e)
+            }
+        }
+
     }
+    Ok(())
 }
 
 fn handle_connection(mut stream: TcpStream) {
